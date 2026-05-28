488301897db533570f365c6a9bec58bb5ff7ba92
Lazaros Arvanitis <lazarosarvanitis@gmail.com>
2026-05-28 13:29:47 +0300

    chore: wording fix

--- src/components/ArrowFunctionalComponent.tsx
+++
@@ -1,5 +0,0 @@
-const ArrowFunctionalComponent = () => {
-    const title = 'I am an Arrow Functional Component';
-    return <h1>{title}</h1>
-}
-export default ArrowFunctionalComponent;
--- src/components/FunctionalComponent.tsx
+++
@@ -1,7 +0,0 @@
-
-function FunctionalComponent() {
-
-    const title = "I am a Functional Component"
-    return <h1>{title}</h1>
-}
-export default FunctionalComponent;
