2385cfa954ecea16b044211a23365b22343035dd
Lazaros Arvanitis <lazarosarvanitis@gmail.com>
2026-05-28 13:32:24 +0300

    feat: add header, footer and layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,75 @@
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ClassComponent from "./components/ClassComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
+// import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
+// import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
+import Layout from "./components/Layout.tsx";
+
+// type PropsA = {
+//   title: string,
+//   description: string,
+// }
+// type PropsB = {
+//   title: string,
+//   description: string,
+// }
+//
+// // UNION
+// type Status = "Error" | "Info" | "Warning";
+//
+// // Tuples
+// type Coordinates = [number, number];
+//
+// //Functions
+// type ClickHandler = (event: MouseEvent) => void;
+//
+// //INTERSECTION
+// type Props = PropsA & PropsB;
+//
+// interface PropsTwo {
+//   title: string,
+//   description: string,
+// }
+//
+// interface PropsTwo {
+//   price: number,
+//   sort: number,
+// }
+// // PropsTwo = {title, description, price, sort}
+//
+// interface PropsThree extends PropsTwo {
+//   image: string,
+// }
+// // PropsThree = {title, description, price, sort, image}
 
 function App() {
 
   return (
-      <>
-      <FunctionalComponent/>
-      <ClassComponent/>
-      <ArrowFunctionalComponent/>
-      </>
+    <>
+      {/*LESSON TWO*/}
+      {/*<FunctionalComponent />*/}
+      {/*<FunctionalComponent />*/}
+      {/*<FunctionalComponent />*/}
+      {/*<FunctionalComponent />*/}
+      {/*<ClassComponent />*/}
+      {/*<ArrowFunctionalComponent />*/}
+
+      {/*LESSON THREE*/}
+      {/*<ArrowFunctionalComponentWithProps title="Heading 1"/>*/}
+      {/*<ArrowFunctionalComponentWithProps*/}
+      {/*  title="Second Title"*/}
+      {/*  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet, deserunt dicta excepturi fugit ipsa modi perferendis quo repellat voluptates."*/}
+      {/*/>*/}
+
+      <Layout addClasses="text-blue-600 bg-gray-100">
+        {/*<ArrowFunctionalComponentWithProps*/}
+        {/*  title="Second Title"*/}
+        {/*  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet, deserunt dicta excepturi fugit ipsa modi perferendis quo repellat voluptates."*/}
+        {/*/>*/}
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, reiciendis.</p>
+      </Layout>
+
+    </>
   )
 }
 
-export default App;
+export default App
